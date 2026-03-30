90b565514a1d0d7f4dc613f4b635172968dd3168
Mathea Gaveriaux <mathea.gaveriaux@gmail.com>
2026-03-30 16:05:27 +0200

    API: Creation de la route api/bikes/stations et intégration des données JSON

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .services import get_formatted_stations
 
-# Create your views here.
+def stations_list(request):
+    """
+    Retourne toutes les stations Vélib' en JSON.
+    Accepte un paramètre GET ?limit=XX
+    """
+    limit = int(request.GET.get("limit", 100))
+    stations = get_formatted_stations(limit=limit)
+    return JsonResponse({"count": len(stations), "stations": stations})
